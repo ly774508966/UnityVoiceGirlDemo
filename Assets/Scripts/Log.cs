9f4e2bfa2ae31d60b9babc6672600672f0e64057
zwxbest <zhangweixiao@live.com>
2017-03-31 17:37:41 +0800

    添加日志类

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Log
+{
+
+    static FileStream fs;
+    static StreamWriter sw;
+
+    public static void Init()
+    {
+        fs = new FileStream("日志.txt", FileMode.Create);
+        sw = new StreamWriter(fs);
+    }
+
+    public static void WriteToLog(string log)
+    {
+        //fs = new FileStream("日志.txt", FileMode.Create);
+        //sw = new StreamWriter(fs);
+        sw.WriteLine(log);
+        sw.Flush();
+        //sw.Close();
+        //fs.Close();
+    }
+
+    public static void DisConnect()
+    {
+        sw.Close();
+        fs.Close();
+    }
+
+
+}
+
